ddce9a07798df8c13b9c77f9c353a643d97fef8c
Denzel <denzelnyatsanza@gmail.com>
2021-07-24 16:04:05 +0200

    Fixed testimonials

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,27 +1,19 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
+    name: 'Shawn Denglesi',
     title: 'CEO',
-    org: 'WEB CIFAR',
+    org: 'Elixer',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'Denzel is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
-    name: 'Fahim Elahe',
-    title: 'Co-founder',
-    org: 'Kreetive',
+    name: 'Natalie Nyatsanza',
+    title: 'Model',
+    org: '',
     desc:
-      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
-  },
-  {
-    id: 3,
-    name: 'Sayed',
-    title: 'Marketing Head',
-    org: 'Eleven Dawn',
-    desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing.',
   },
 ];
 export default testimonials;
