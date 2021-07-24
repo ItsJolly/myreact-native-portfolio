c0e8380a39cc91b8eae27e6716e46d51e1410de8
Denzel <denzelnyatsanza@gmail.com>
2021-07-24 16:48:13 +0200

    Social Links updated

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -175,7 +175,7 @@ export default function HeroSection() {
               <ul>
                 <li>
                   <a
-                    href="http://facebook.com/webcifar"
+                    href="http://facebook.com/officialjollyy"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -184,7 +184,7 @@ export default function HeroSection() {
                 </li>
                 <li>
                   <a
-                    href="http://twitter.com/webcifar"
+                    href="http://twitter.com/officialjollyy"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -193,7 +193,7 @@ export default function HeroSection() {
                 </li>
                 <li>
                   <a
-                    href="http://isntagram.com/web_cifar"
+                    href="http://isntagram.com/officialjollyy"
                     target="_blank"
                     rel="noreferrer"
                   >
